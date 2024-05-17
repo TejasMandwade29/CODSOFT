4727a62641ddca11c65464ce417b91cc2d202fb8
Tejas Mandwade <mandawadeteju@gmail.com>
2024-05-17 11:01:11 +0530

    Add files via upload

---
+++ python Task 2.py
@@ -0,0 +1,41 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+print("A Simple Calculator")
+
+
+print("Operations:")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+choice = input("Enter operation number (1-4): ")
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if choice == "1":
+    result = add(num1, num2)
+    operation = "+"
+elif choice == "2":
+    result = subtract(num1, num2)
+    operation = "-"
+elif choice == "3":
+    result = multiply(num1, num2)
+    operation = "*"
+elif choice == "4":
+    result = divide(num1, num2)
+    operation = "/"
+
+
+print(f"{num1} {operation} {num2} = {result}")
